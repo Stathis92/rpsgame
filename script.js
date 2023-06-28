a9412eb6a056be79729d7f7460ec33b571ce25f2
Stathis <133227039+Stathis92@users.noreply.github.com>
2023-06-28 20:07:13 +0300

    Create random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Script Loaded");
+//Randomizes computer choice
+function getComputerChoice() {
+    let randomSelect = Math.floor(Math.random()*choices.length);
+    return choices[randomSelect];
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    return;
+  }
+  
+  const choices = ["Rock", "Paper", "Scissors"];
+  const playerSelection = "rock";
+
+  const computerSelection = getComputerChoice();
+  console.log("Computer Choice: "+ computerSelection);
