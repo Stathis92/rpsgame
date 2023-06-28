4fd19e36a62416ba981e31cfedcdbd52aa09575c
Stathis <133227039+Stathis92@users.noreply.github.com>
2023-06-28 20:46:53 +0300

    Add playerSelection case function

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 //Randomizes computer choice
 function getComputerChoice() {
-    let randomSelect = Math.floor(Math.random()*choices.length);
-    return choices[randomSelect];
+  let randomSelect = Math.floor(Math.random() * choices.length);
+  return choices[randomSelect];
+}
+//Fix the playerSelection case
+function playerSensitive(playerSelection) {
+  result =
+    playerSelection.charAt(0).toUpperCase() +
+    playerSelection.slice(1).toLowerCase();
+  return result;
 }
 
-
 function playRound(playerSelection, computerSelection) {
-    // your code here!
-    return;
-  }
-  
-  const choices = ["Rock", "Paper", "Scissors"];
-  const playerSelection = "rock";
+  // your code here!
+  return;
+}
 
-  const computerSelection = getComputerChoice();
-  console.log("Computer Choice: "+ computerSelection);
+const choices = ["Rock", "Paper", "Scissors"];
+let playerSelection = prompt("Select:\nRock\nPaper\nScissor");
+playerSelection = playerSensitive(playerSelection);
+
+const computerSelection = getComputerChoice();
+console.log("Computer Choice Was: " + computerSelection);
+console.log("Player Choice Was: " + playerSelection);
